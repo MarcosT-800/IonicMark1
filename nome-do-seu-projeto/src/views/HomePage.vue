289b565f84d74998d2a7ce04175b4809e65d40ef
Marcos Alves <marcosalvesjr007@gmail.com>
2024-02-18 01:26:41 +0000

    aaaaa

--- nome-do-seu-projeto/src/views/HomePage.vue
+++ nome-do-seu-projeto/src/views/HomePage.vue
@@ -19,7 +19,23 @@
       </ion-toolbar>
     </ion-header>
     <ion-content class="ion-padding">
-      <Content />
+      <ion-list>
+    <ion-item>
+      <ion-label>Pokémon Yellow</ion-label>
+    </ion-item>
+    <ion-item>
+      <ion-label>Mega Man X</ion-label>
+    </ion-item>
+    <ion-item>
+      <ion-label>The Legend of Zelda</ion-label>
+    </ion-item>
+    <ion-item>
+      <ion-label>Pac-Man</ion-label>
+    </ion-item>
+    <ion-item>
+      <ion-label>Super Mario World</ion-label>
+    </ion-item>
+  </ion-list>
     </ion-content>
   </ion-page>
 </template>
